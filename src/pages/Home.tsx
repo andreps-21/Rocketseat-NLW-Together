fd639496178045b40e6c9b68e02a0d975d82d105
andreps-21 <andrepsss@gmail.com>
2021-06-22 23:29:13 -0300

    Letmeask - Autenticação Firebase

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,28 @@
+
 //importação de imagens no react 
 import { useHistory } from 'react-router-dom'
+
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg';
 
-import '../styles/auth.scss';
 import { Button } from '../components/Button';
+
+import '../styles/auth.scss';
+import { useAuth } from '../hooks/useAuth';
+
 
 export function Home() {
     const history = useHistory();
-    function navigateToNewRoom() {
-        history.push('/rooms/new')
-    }
+    const { user, signInWithGoogle } = useAuth()
 
+    async function handleCreateRoom() {
+        if (!user) {
+            await signInWithGoogle()
+        }
+
+        history.push('/rooms/new');
+    }
 
     return (
         <div id="page-auth">
@@ -24,7 +34,7 @@ export function Home() {
             <main>
                 <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
-                    <button onClick={navigateToNewRoom} className="create-room">
+                    <button onClick={handleCreateRoom} className="create-room">
                         <img src={googleIconImg} alt="Logo do Google" />
                         Crie sua sala com o Google
                     </button>
