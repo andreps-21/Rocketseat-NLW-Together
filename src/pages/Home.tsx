f7a89e0496dbdbb162cb689ceb91aa622c3f0c14
andreps-21 <andrepsss@gmail.com>
2021-06-25 08:14:10 -0300

    Letmask - Integração BD

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,55 @@
 
 //importação de imagens no react 
 import { useHistory } from 'react-router-dom'
+import { FormEvent, useState } from 'react';
+
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg';
 
+import { database } from '../services/firebase';
+
 import { Button } from '../components/Button';
 
-import '../styles/auth.scss';
 import { useAuth } from '../hooks/useAuth';
+
+import '../styles/auth.scss';
+
+
+
+
 
 
 export function Home() {
     const history = useHistory();
     const { user, signInWithGoogle } = useAuth()
-
+    const [roomCode, setRoomCode] = useState('');
+    // verifica se o usuário existe
     async function handleCreateRoom() {
         if (!user) {
             await signInWithGoogle()
         }
 
         history.push('/rooms/new');
+    }
+
+    async function handleJoinRoom(event: FormEvent) {
+        event.preventDefault();
+
+        if (roomCode.trim() === '') {
+            return;
+        }
+
+        const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+        if (!roomRef.exists()) {
+            alert('Esta sala não existe! Tente novamente.');
+            return;
+        }
+
+        history.push(`/rooms/${roomCode}`);
+
     }
 
     return (
@@ -39,10 +67,12 @@ export function Home() {
                         Crie sua sala com o Google
                     </button>
                     <div className="separator">ou entre em uma sala</div>
-                    <form>
+                    <form onSubmit={handleJoinRoom}>
                         <input
                             type="text"
                             placeholder="Digite o código da sala"
+                            onChange={event => setRoomCode(event.target.value)}
+                            value={roomCode}
                         />
                         <Button type="submit">
                             Entrar na sala
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,17 +1,41 @@
 
 //importação de imagens no react 
-import { Link } from 'react-router-dom'
+import { FormEvent, useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 
 import '../styles/auth.scss';
 import { Button } from '../components/Button';
-//import { useAuth } from '../hooks/useAuth';
+import { database } from '../services/firebase';
+import { useAuth } from '../hooks/useAuth';
+
 
 
 export function NewRoom() {
-    //const { user } = useAuth;
+    const { user } = useAuth()
+    const history = useHistory()
+    const [newRoom, setNewRoom] = useState('');
 
+
+    async function handleCreateRoom(event: FormEvent) {
+        event.preventDefault();
+
+        // retira os espaços caso o usuário dê ao escrever o nome da sala
+        if (newRoom.trim() === '') {
+            return;
+        }
+
+        const roomRef = database.ref('rooms');
+
+        const firebaseRoom = await roomRef.push({
+            title: newRoom,
+            authorId: user?.id
+
+        })
+
+        history.push(`/rooms/${firebaseRoom.key}`)
+    }
 
     return (
         <div id="page-auth">
@@ -24,10 +48,12 @@ export function NewRoom() {
                 <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
                     <h2>Criar uma nova sala</h2>
-                    <form>
+                    <form onSubmit={handleCreateRoom}>
                         <input
                             type="text"
                             placeholder="nome da sala"
+                            onChange={event => setNewRoom(event.target.value)}
+                            value={newRoom}
                         />
                         <Button type="submit">
                             Criar sala
