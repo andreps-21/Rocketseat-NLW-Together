323c7f1167ef7a47edaffe2041658da1a33a27fe
andreps-21 <andrepsss@gmail.com>
2021-06-30 09:23:29 -0300

    Letmeask - Button Like

---
+++ src/hooks/useRoom.ts
@@ -0,0 +1,70 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { database } from "../services/firebase";
+import { useAuth } from "./useAuth";
+
+type FirebaseQuestions = Record<string, {
+    author: {
+        name: string;
+        avatar: string;
+    }
+
+    content: string;
+    isAnswered: boolean;
+    isHIghLighted: boolean;
+    likes: Record<string, {
+        authorId: string;
+    }>
+}>
+
+type QuestionsType = {
+    id: string;
+    author: {
+        name: string;
+        avatar: string;
+    }
+    content: string;
+    isAnswered: boolean;
+    isHIghLighted: boolean;
+    likeCount: number;
+    hasLiked: boolean;
+
+}
+
+export function useRoom(roomId: string) {
+    const { user } = useAuth();
+
+    const [questions, setQuestions] = useState<QuestionsType[]>([]);
+    const [title, setTitle] = useState();
+
+
+
+    useEffect(() => {
+        const roomRef = database.ref(`rooms/${roomId}`);
+
+        roomRef.on('value', room => {
+            const databaseRoom = room.val()
+            const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
+
+            const parsedQuestions = Object.entries(firebaseQuestions).map(([key, value]) => {
+                return {
+                    id: key,
+                    content: value.content,
+                    author: value.author,
+                    isHIghLighted: value.isHIghLighted,
+                    isAnswered: value.isAnswered,
+                    likeCount: Object.values(value.likes ?? {}).length,
+                    hasLiked: Object.values(value.likes ?? {}).some(like => like.authorId === user?.id)
+                }
+            })
+            setTitle(databaseRoom.title);
+            setQuestions(parsedQuestions)
+        })
+        return () => {
+            roomRef.off('value');
+        }
+    }, [roomId, user?.id]);
+
+    return { questions, title }
+
+}
